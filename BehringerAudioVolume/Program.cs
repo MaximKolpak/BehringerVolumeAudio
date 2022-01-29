806803ec4ce7ba92121ba9e12507c914ef14dd58
Maxim Kolpakov <max.kolpakov.st@gmail.com>
2022-01-29 10:47:34 +0200

    On branch main

---
+++ BehringerAudioVolume/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BehringerAudioVolume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
